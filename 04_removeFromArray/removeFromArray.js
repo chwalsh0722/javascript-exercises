ed4885743d0bab034e24727518cb266f61d99635
Cullen <lilspikeyc@gmail.com>
2023-03-26 15:56:17 -0500

    Completed problems 4 - 6

--- 04_removeFromArray/removeFromArray.js
+++ 04_removeFromArray/removeFromArray.js
@@ -1,5 +1,43 @@
-const removeFromArray = function() {
+const removeFromArray = function(array, ...itemsToRemove) {
+  let arrayLength = array.length;
+  let numRemovals = itemsToRemove.length;
+  let newArray = [];
+  let removeItem = false;
 
+  for (i = 0; i < arrayLength; i++)
+  {
+    removeItem = false;
+    for (j = 0; j < numRemovals; j++)
+    {
+      if (array[i] === itemsToRemove[j])
+      {
+        removeItem = true;
+      }
+    }
+    if (removeItem)
+    {
+      continue;
+    }
+    else
+    {
+      newArray.push(array[i]);
+    }
+  }
+  
+  // for (let i = 0; i < arrayLength; i++)
+  // {
+  //   for (let j = 0; j < numRemovals; j++)
+  //   if (array[i] == itemToRemove[0] || array[i] == itemToRemove[1])
+  //   {
+  //     continue;
+  //   }
+  //   else
+  //   {
+  //     newArray.push(array[i]);
+  //   }
+  // }
+
+  return newArray;
 };
 
 // Do not edit below this line
--- 05_sumAll/sumAll.js
+++ 05_sumAll/sumAll.js
@@ -1,5 +1,34 @@
-const sumAll = function() {
+const sumAll = function(num1, num2) {
+  let largeNumber = 0;
+  let smallNumber = 0;
+  let sum = 0;
 
+  if (num1 < 0 || num2 < 0 || typeof num1 != 'number' || typeof num2 != 'number')
+  {
+    return 'ERROR';
+  }
+
+  if (num1 < num2)
+  {
+    largeNumber = num2;
+    smallNumber = num1;
+  }
+  else if (num1 > num2)
+  {
+    largeNumber = num1;
+    smallNumber = num2;
+  }
+  else
+  { 
+    return 'ERROR'
+  }
+
+  for (let i = smallNumber; i <= largeNumber; i++)
+  {
+    sum += i;
+  }
+
+  return sum;
 };
 
 // Do not edit below this line

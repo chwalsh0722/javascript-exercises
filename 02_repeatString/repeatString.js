2b268d7bb1e6dd47a15922e27890bcd0cb0bc07d
Cullen <lilspikeyc@gmail.com>
2023-03-26 14:07:10 -0500

    Completed exercises 1 - 3

--- 02_repeatString/repeatString.js
+++ 02_repeatString/repeatString.js
@@ -1,5 +1,23 @@
-const repeatString = function() {
+const repeatString = function(string, num) {
 
+  let newString = string;
+
+  if (num == 0)
+  {
+    return '';
+  }
+  else if (num < 0)
+  {
+    return 'ERROR';
+  }
+  else
+  {
+    for (let i = 0; i < num - 1; i++)
+    {
+      newString += string;
+    }
+    return newString;
+  }
 };
 
 // Do not edit below this line
--- 03_reverseString/reverseString.js
+++ 03_reverseString/reverseString.js
@@ -1,5 +1,11 @@
-const reverseString = function() {
-
+const reverseString = function(string) {
+  let strLength = string.length;
+  let reverseString = string.charAt(strLength - 1);
+  for (let i = strLength - 2; i >= 0; i--)
+  {
+    reverseString += string.charAt(i);
+  }
+  return reverseString;
 };
 
 // Do not edit below this line
